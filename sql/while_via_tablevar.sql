fce4322878c2f9628b49cdb7a574384eb3cc016e
Gogs <gogs@fake.local>
2020-01-28 17:19:52 +0500

    Some changes

---
+++ sql/while_via_tablevar.sql
@@ -0,0 +1,17 @@
+﻿DECLARE @t TABLE (N INT, ID INT);
+
+INSERT INTO @t (N, ID)
+  VALUES (1,1),(2,2),(3,3),(4,4),(5,5),(6,6);
+
+/*SELECT ROW_NUMBER() over (order by id) AS I, N, ID
+  FROM @t;*/
+
+DECLARE @n INT = 1, @c INT, @v INT;
+
+SELECT @c = COUNT(*) FROM @t;
+
+WHILE @n <= @c
+BEGIN
+  SELECT @v = ID, @n += 1 FROM @t WHERE N = @n;
+  PRINT @v;
+END
