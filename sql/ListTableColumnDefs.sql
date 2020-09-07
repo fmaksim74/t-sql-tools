246b0e9e3d4ba754feeda9e2ad4c31f57d8339ca
FedorchenkoMI <FedorchenkoMI@uralprombank.ru>
2020-09-07 14:25:34 +0500

    Query to list tables columns definintions.

---
+++ sql/ListTableColumnDefs.sql
@@ -0,0 +1,8 @@
+﻿SELECT ColumnName = LTRIM(RTRIM(CONVERT(VARCHAR, d.ColumnName))),
+       ColumnType = LTRIM(RTRIM(CONVERT(VARCHAR, d.ColumnType))),
+       LineEnd = ','
+  FROM            sys.tables      AS t
+       INNER JOIN sys.all_columns As c ON c.object_id = t.object_id
+       OUTER APPLY dbo.stf_getColumnTypeDefinition(t.name, c.name, t.type) d
+WHERE t.name = 'tmp_BookOfSale'
+
